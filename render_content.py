2760a53b2a3fe5d5a674154c6cc280cf7912ea69
janus <janus@insignificancegalore.net>
2021-06-23 14:08:42 +0200

    Implement subfolder support
    This version is working when served over http, but fails to highligt active entry when served from static files.

--- render_content.py
+++ render_content.py
@@ -1,20 +1,20 @@
 from jinja2 import Template
 from html.parser import HTMLParser
+import os
 from pathlib import Path
 
 
-class HrefParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.targets = []
-        
-    def handle_starttag(self, tag, attrs):
-        if tag=='a':
-            for v in (v for k,v in attrs if k=='href'):
-                self.targets.append(v)
-
-
 def get_file_names():
+    class HrefParser(HTMLParser):
+        def __init__(self):
+            super().__init__()
+            self.targets = []
+            
+        def handle_starttag(self, tag, attrs):
+            if tag=='a':
+                for v in (v for k,v in attrs if k=='href'):
+                    self.targets.append(v)
+
     p = Path('_navbar.html')
     h = HrefParser()
     h.feed(p.read_text())
@@ -25,7 +25,13 @@ def main():
     t = Template(Path('full_content_template.html').read_text())
 
     for href in (Path(f) for f in get_file_names()):
-        href.write_text(t.render(title=href.stem.replace('_', ' ')))
+        href.parent.mkdir(parents=True,exist_ok=True)
+        root_rel = os.path.relpath('.', href.parent)
+        print(f"Writing {href}. Root rel: {root_rel}")
+        href.write_text(t.render(
+            title=href.stem.replace('_', ' '),
+            root_rel=root_rel,
+        ))
 
 
 if __name__ == '__main__':
